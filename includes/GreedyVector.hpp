21ba448a82f2044a799af8f157ef74393b3cb031
sergio15dev <sergio15.dev@gmail.com>
2023-12-29 02:04:31 -0500

    fix greedy

--- includes/GreedyVector.hpp
+++ includes/GreedyVector.hpp
@@ -88,6 +88,37 @@ class GreedyVector {
             return knns;
         }
 
+        vector<vector<double>> KNNBruteForce3(const vector<double>& query_point, int k) const {
+            // Verificar que haya nodos antes de realizar la búsqueda
+            if (nodes.empty()) {
+                cerr << "Error: No hay nodos para realizar la búsqueda KNN." << endl;
+                return {};
+            }
+
+            // Calcular distancias y almacenarlas en un vector de pares (distancia, índice)
+            vector<pair<double, double>> distances;
+            for (double i = 0; i < nodes.size(); i++) {
+                double distance = 0.0;
+                for (int d = 0; d < dimension; d++) {
+                    double diff = nodes[i].point[d] - query_point[d];
+                    distance += diff * diff;
+                }
+                distances.emplace_back(sqrt(distance), i);
+            }
+
+            // Ordenar el vector de distancias por la distancia ascendente
+            sort(distances.begin(), distances.end());
+
+            // Recopilar los k vecinos más cercanos
+            vector<vector<double>> k_nearest_neighbors;
+            for (int i = 0; i < k && i < distances.size(); i++) {
+                double index = distances[i].second;
+                k_nearest_neighbors.push_back(nodes[index].point);
+            }
+
+            return k_nearest_neighbors;
+        }
+
     protected:
         double distanceL2(const vector<double>& p1, const vector<double>& p2) {
             double distancia = 0.0;
--- tester.cpp
+++ tester.cpp
@@ -1,5 +1,6 @@
 #include "includes/KDTree.hpp"
 #include "includes/Kmeans.hpp"
+#include "includes/GreedyVector.hpp"
 #include "utils/Reader.hpp"
 #include <iostream>
 #include <vector>
@@ -7,6 +8,7 @@
 using namespace std;
 using namespace rdr;
 using namespace kdt;
+using namespace grd;
 
 
 
@@ -23,11 +25,20 @@ int main(){
     for (auto &row : points){
         kdtree.insert(row);
     }
-
     vector<Point2D> knns = kdtree.searchKNN2({0,0}, 1);
     for(int i=0; i<knns.size(); i++){
         cout<<knns[i]<<endl;
     }
 
+    cout<<"----------------"<<endl;
+
+    GreedyVector<2> greedy;
+    for(auto &row : points){
+        greedy.insert(row);
+    }
+    vector<Point2D> knns2 = greedy.KNNBruteForce3({0,0}, 1);
+    for(int i=0; i<knns2.size(); i++){
+        cout<<knns2[i]<<endl;
+    }
     return 0;
 }
